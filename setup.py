77e94d647a4b7ac1a48f33b570d1e3d95289a434
Alisher Ibragimov <AlisherBlackMSU@yandex.ru>
2026-04-02 15:47:18 +0300

    flag to True

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ LINE_INFO = os.getenv("LINE_INFO", "0") == "1"
 def get_ext():
     from torch.utils.cpp_extension import BuildExtension
 
-    return BuildExtension.with_options(no_python_abi_suffix=True, use_ninja=False)
+    return BuildExtension.with_options(no_python_abi_suffix=True, use_ninja=True)
 
 
 def get_extensions():
